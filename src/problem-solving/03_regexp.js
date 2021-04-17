0b725bf2ec804e4c94711d0aaffbd8dc2719a85e
DevelopmenTom <tomgivol@gmail.com>
2021-04-17 22:14:07 +0200

    feat: solve regex part

--- src/problem-solving/03_regexp.js
+++ src/problem-solving/03_regexp.js
@@ -1,5 +1,28 @@
-/**
- * This test, tests some knowledge of Regular Expressions.
- *
- * See the test for what functions you need to implement.
- */
+const answers = {
+  captureThreeNumbers: (string) => {
+    const threeNumbers = string.match(/(\d\d\d)/)
+    return threeNumbers === null ? false : threeNumbers[0]
+  },
+
+  containsNumber: (string) => {
+    return /.*\d.*/.test(string)
+  },
+
+  containsRepeatingLetter: (string) => {
+    return /.*([a-zA-Z]).*\1/.test(string)
+  },
+
+  endsWithVowel: (string) => {
+    return /.*[aeiouAEIOU]$/.test(string)
+  },
+
+  isUSD: (string) => {
+    return /^\$\d+(\.|\,)\d*(\1?\d{3})?\.?(\d{2})?$/.test(string)
+  },
+
+  matchesPattern: (string) => {
+    return /^\d{3}\-\d{3}\-\d{4}$/.test(string)
+  }
+}
+
+export default answers
