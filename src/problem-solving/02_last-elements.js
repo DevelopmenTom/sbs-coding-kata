a2fa1b6a706c1df8f85f200880906a4ab5bc4595
DevelopmenTom <tomgivol@gmail.com>
2021-04-17 20:28:31 +0200

    feat: solve last elements exercise

--- src/problem-solving/02_last-elements.js
+++ src/problem-solving/02_last-elements.js
@@ -1,5 +1,22 @@
-/**
- * Get the last n data elements from the nested object
- *
- * See the test if you have questions
- */
+export const getLastNumbers = (payload, amountToExtract) => {
+  const allData = []
+
+  const extractData = (object) => {
+    allData.push(object.data)
+
+    if (object.next) {
+      return extractData(object.next)
+    }
+    return allData
+  }
+
+  const extractedData = extractData(payload)
+  if (amountToExtract >= extractedData.length) {
+    return extractedData
+  }
+
+  return extractedData.slice(
+    extractedData.length - amountToExtract,
+    extractedData.length
+  )
+}
