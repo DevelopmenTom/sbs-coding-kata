183b685cc10856d0da140e2164a6f663578ff0d1
DevelopmenTom <tomgivol@gmail.com>
2021-04-17 18:46:50 +0200

    feat: solve first question

--- src/problem-solving/01_arrays.js
+++ src/problem-solving/01_arrays.js
@@ -1,3 +1,6 @@
 /**
  * Have a look at the test and implement the needed function, so the test will succeed
  */
+export const sum = (arr) => {
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
