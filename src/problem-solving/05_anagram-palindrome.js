a871d949e9128aaf2532286491d24385d9871db1
DevelopmenTom <tomgivol@gmail.com>
2021-04-18 00:19:00 +0200

    feat: solve palindrome section

--- src/problem-solving/05_anagram-palindrome.js
+++ src/problem-solving/05_anagram-palindrome.js
@@ -8,3 +8,21 @@
  * Constraints
  * contains only lowercase letters in the range ascii[a..z]
  */
+
+export const isPalindromePossible = (string) => {
+  const characterDictonary = {}
+  for (const character of string) {
+    characterDictonary[character]
+      ? characterDictonary[character]++
+      : (characterDictonary[character] = 1)
+  }
+
+  let oddCharacters = 0
+  for (const character in characterDictonary) {
+    if (characterDictonary[character] % 2 !== 0) {
+      oddCharacters++
+    }
+  }
+
+  return oddCharacters < 2
+}
