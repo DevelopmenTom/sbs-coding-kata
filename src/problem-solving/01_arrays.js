9d718f6d5494573fc8d63fcdb00708a67c521f04
DevelopmenTom <tomgivol@gmail.com>
2021-04-17 20:07:57 +0200

    feat: solve rest of array questions

--- src/problem-solving/01_arrays.js
+++ src/problem-solving/01_arrays.js
@@ -1,6 +1,31 @@
-/**
- * Have a look at the test and implement the needed function, so the test will succeed
- */
 export const sum = (arr) => {
   return arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
+
+export const concat = (...arrays) => {
+  let product = []
+  for (const array of arrays) {
+    product = product.concat(array)
+  }
+  return product
+}
+
+export const count = (array, itemToCount) => {
+  const occurances = array.filter((item) => item === itemToCount)
+  return occurances.length
+}
+
+export const duplicates = (array) => {
+  let duplicates = []
+  const sortedArray = [...array].sort()
+  for (let i = 0; i < sortedArray.length; i++) {
+    if (sortedArray[i] === sortedArray[i + 1]) {
+      duplicates.push(sortedArray[i])
+    }
+  }
+  return [...new Set(duplicates)]
+}
+
+export const square = (array) => {
+  return array.map((item) => item * item)
 }
